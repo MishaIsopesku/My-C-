0b7f9eaee5ebb0a1c1aeca4fd2b370dd2801a27d
Misha Isopesku <78026214+MishaIsopesku@users.noreply.github.com>
2022-01-27 13:22:38 +0200

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int v = 654;
+            int x = (int)Math.Log10(v) + 1;
+            int result = 0;
+            for (int i = 0; i < x; i++)
+            {
+                result = result * 10 + (v % 10);
+                v /= 10;
+            }
+            Console.WriteLine("Результат");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
