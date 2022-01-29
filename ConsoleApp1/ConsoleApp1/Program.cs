22d9298e582c9dc95d5eb2920c759cc76b1d04de
Misha Isopesku <78026214+MishaIsopesku@users.noreply.github.com>
2022-01-29 16:31:33 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,16 +1,22 @@
 using System;
 
-namespace Program1{ 
-    
-    class Program{
-        
-    public static void Main(){
-        
-    while (true) { Console.Write("n » ");
-    int n = int.Parse(Console.ReadLine());
-    n -= 99 * (n / 100 - n % 10);
-    Console.WriteLine(n); 
-   } 
-  } 
- } 
+namespace Program1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            while (true)
+            {
+                Console.Write("Введіть число » ");
+                int n = int.Parse(Console.ReadLine());
+                n -= 99 * (n / 100 - n % 10);
+                
+                Console.WriteLine("\r");
+                Console.WriteLine("Результат");
+                Console.WriteLine(n);
+                
+            }
+        }
+    }
 }
