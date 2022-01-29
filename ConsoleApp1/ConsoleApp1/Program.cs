33fb07380617d1faad61814354324cb07f885aac
Misha Isopesku <78026214+MishaIsopesku@users.noreply.github.com>
2022-01-29 16:20:39 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,22 +1,16 @@
-﻿using System;
+using System;
 
-namespace Program1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int v = 654;
-            int x = (int)Math.Log10(v) + 1;
-            int result = 0;
-            for (int i = 0; i < x; i++)
-            {
-                result = result * 10 + (v % 10);
-                v /= 10;
-            }
-            Console.WriteLine("Результат");
-            Console.WriteLine(result);
-            Console.ReadKey();
-        }
-    }
+namespace Program1{ 
+    
+    class Program{
+        
+    public static void Main(){
+        
+    while (true) { Console.Write("n » ");
+    int n = int.Parse(Console.ReadLine());
+    n -= 99 * (n / 100 - n % 10);
+    Console.WriteLine(n); 
+   } 
+  } 
+ } 
 }
