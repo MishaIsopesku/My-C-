421ebad911b7d6bb060b08bbc6c7e3ccbd537928
Misha Isopesku <78026214+MishaIsopesku@users.noreply.github.com>
2022-03-15 09:06:03 +0200

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,22 +1,41 @@
 using System;
- 
-namespace Program1
+using System.Text;
+
+namespace NumbReverseNet50
 {
-    class Program
+    public static class NumbReverse
     {
-        static void Main(string[] args)
+        public static int Reverse(int n)
         {
-            int v = 654;
-            int x = (int)Math.Log10(v) + 1;
+            int x = (int)Math.Log10(n) + 1;
             int result = 0;
             for (int i = 0; i < x; i++)
             {
-                result = result * 10 + (v % 10);
-                v /= 10;
+                result = result * 10 + (n % 10);
+                n /= 10;
             }
-            Console.WriteLine("Результат");
-            Console.WriteLine(result);
-            Console.ReadKey();
+
+            Console.Write("\n Ваші числа наоборот: {0}", result);
+            return result;
         }
+
+        /** 
+        * Виведення результату програми
+        */
+        static void Main()
+        {
+            
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.Write("\n Напишіть свої числа: ");
+            string S1 = Console.ReadLine();
+            int v = int.Parse(S1);
+            Reverse(v);
+
+            Console.Write("\n Нажміть будь-яку літеру щоб вийти: ");
+            Console.ReadLine();
+
+        }
+
+   
     }
 }
